19fd42f1e7fbcc364fa1e0067d66377fc9cbf232
yunyi <1154730091@qq.com>
2025-08-21 18:41:32 +0800

    int:Dockerfile

--- src/start.sh
+++ src/start.sh
@@ -7,13 +7,13 @@ SERVE_API_LOCALLY="${SERVE_API_LOCALLY:-false}"
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --listen --log-stdout &
+    python3 -u /comfyui/main.py --disable-auto-launch --disable-metadata --listen --log-stdout &
 
     echo "worker-comfyui: Starting RunPod Handler"
-    python -u /handler.py --rp_serve_api --rp_api_host=0.0.0.0
+    python3 -u /handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
-    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --log-stdout &
+    python3 -u /comfyui/main.py --disable-auto-launch --disable-metadata --log-stdout &
 
     echo "worker-comfyui: Starting RunPod Handler"
-    python -u /handler.py
+    python3 -u /handler.py
 fi
